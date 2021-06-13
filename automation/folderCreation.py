feebc8e54ceb7dd99920c271426481a762ed6429
RemcoHalman <remco.halman@gmail.com>
2021-06-13 11:55:59 +0200

    fix: indexError catched

--- automation/folderCreation.py
+++ automation/folderCreation.py
@@ -6,14 +6,19 @@ def createFolder(name, path):
     if path != ".":
         os.chdir(path)
     os.mkdir(foldername)
-    print(f"Folder {name} created successfully")
+    print(f"Folder '{name}' created successfully")
 
 
 if __name__ == '__main__':
     cwd = os.getcwd()
-    foldername = sys.argv[1]
-    if len(sys.argv) == 3:
-        path = sys.argv[2]
-    else:
-        path = "."
-    createFolder(foldername, path)
+    try:
+        foldername = sys.argv[1]
+        if len(sys.argv) == 3:
+            path = sys.argv[2]
+        else:
+            path = "."
+        createFolder(foldername, path)
+    except IndexError:
+        print("!! Please enter a foldername !!")
+
+
